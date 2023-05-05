a7c11a4abb6f08deb6364ebcf5d095648370ff3b
Emily Davidson <ejdavidson777@gmail.com>
2023-05-05 11:14:51 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+# -- INTERFACE TABS -- #
+ interface_img_path =
+
+# - Backstory - #
+
+# - Character Sheet - #
+
+# - Inventory - #
+
+# - Quests - #
+
+# - Spells & Abilities - #
+
+# - Interactive Map - #
+
+
